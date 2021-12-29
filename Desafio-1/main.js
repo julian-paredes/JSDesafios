c0d1137e0c90996f84d6622803d71116a0bb130d
Julian Paredes <julikmet24@gmail.com>
2021-12-29 14:16:29 -0300

    Se suben Desafios 1 - Crear algoritmo simple

---
+++ Desafio-1/main.js
@@ -0,0 +1,9 @@
+// Desafio 1 - JS - Se ingresan dos numeros, se suman y se muestra el resultado //
+ 
+let number1 = Number (prompt('Ingrese el primer número para sumar:'));
+let number2 = Number (prompt('Ingrese el segundo número para sumar:'));
+
+let sum = number1 + number2;
+
+console.log (`El resultado de la suma es ${sum}`);
+alert(`El resultado de la suma es ${sum}`);
