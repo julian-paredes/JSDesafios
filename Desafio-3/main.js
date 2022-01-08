ed5656aaf641c24953bf1b5c11eb8d7ee4204449
Julian Paredes <julikmet24@gmail.com>
2022-01-08 00:32:26 -0300

    Se sube Desafio 3 - Crear algoritmo utilizando un ciclo

---
+++ Desafio-3/main.js
@@ -0,0 +1,10 @@
+// Desafio 3 JS - Bucles
+
+let numero = Number (prompt("Ingrese un número:"));
+
+for (let i=1; i<=10; i++){
+
+    let resultado = numero*i; // Tablas de multiplicar
+    console.log(numero + "x" + i + "=" + resultado);
+    alert(numero + "x" + i + "=" + resultado);
+}
